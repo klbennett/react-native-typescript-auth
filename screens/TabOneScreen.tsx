520c5b76697a1e28d0dbba958f6d4f4a21383699
Kay Bennett <kaybennett@kbanddigital.lan>
2020-07-17 18:43:22 +0100

    google sign in working

--- screens/TabOneScreen.tsx
+++ screens/TabOneScreen.tsx
@@ -1,32 +1,98 @@
-import * as React from 'react';
-import { StyleSheet } from 'react-native';
+import React, { useEffect, useState } from "react";
+import * as Google from "expo-google-app-auth";
 
-import EditScreenInfo from '../components/EditScreenInfo';
-import { Text, View } from '../components/Themed';
+import EditScreenInfo from "../components/EditScreenInfo";
+import { Text, View } from "../components/Themed";
+import { ActivityIndicator, StyleSheet, FlatList, Button } from "react-native";
 
 export default function TabOneScreen() {
+  const [isLoading, setLoading] = useState(true);
+  const [data, setData] = useState([]);
+  const [auth, setAuth] = useState({});
+
+  console.log(data);
+
+  const signIn = async () => {
+    try {
+      const result = await Google.logInAsync({
+        androidClientId:
+          "383140547133-cpsjb6nmdv80ksd133q6i329jvlaes59.apps.googleusercontent.com",
+        //iosClientId: YOUR_CLIENT_ID_HERE,  <-- if you use iOS
+        scopes: ["profile", "email"],
+      });
+
+      if (result.type === "success") {
+        setAuth({
+          signedIn: true,
+          name: result.user.name,
+          photoUrl: result.user.photoUrl,
+        });
+      } else {
+        console.log("cancelled");
+      }
+    } catch (e) {
+      console.log("error", e);
+    }
+  };
+
+  // useEffect(() => {
+  //   signIn();
+  // }, []);
+
   return (
     <View style={styles.container}>
-      <Text style={styles.title}>Tab One</Text>
-      <View style={styles.separator} lightColor="#eee" darkColor="rgba(255,255,255,0.1)" />
-      <EditScreenInfo path="/screens/TabOneScreen.tsx" />
+      {auth.signedIn ? (
+        <LoggedInPage name={auth.name} photoUrl={auth.photoUrl} />
+      ) : (
+        <LoginPage signIn={signIn} />
+      )}
     </View>
   );
 }
 
+const LoginPage = (props) => {
+  return (
+    <View>
+      <Text style={styles.header}>Sign In With Google</Text>
+      <Button title="Sign in with Google" onPress={() => props.signIn()} />
+    </View>
+  );
+};
+
+const LoggedInPage = (props) => {
+  return (
+    <View style={styles.container}>
+      <Text style={styles.header}>
+        Welcome: {props.name}! You are signed in.
+      </Text>
+    </View>
+  );
+};
+
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    alignItems: 'center',
-    justifyContent: 'center',
+    alignItems: "center",
+    justifyContent: "center",
   },
   title: {
     fontSize: 20,
-    fontWeight: 'bold',
+    fontWeight: "bold",
   },
   separator: {
     marginVertical: 30,
     height: 1,
-    width: '80%',
+    width: "80%",
+  },
+  header: {
+    fontSize: 25,
+  },
+  image: {
+    marginTop: 15,
+    width: 150,
+    height: 150,
+    borderColor: "rgba(0,0,0,0.2)",
+    borderWidth: 3,
+    borderRadius: 150,
   },
 });
